fd29bc6c7ac0cd7e852006cfb558d44d0fc69ef2
Illuminatikum <leon.richli@bbzbl-it.ch>
2021-07-02 11:09:51 +0200

    Auf Maven Version geupdatet

---
+++ workspace_M154_knw/MORI/src/main/java/MotoRichi/MORI/App.java
@@ -0,0 +1,13 @@
+package MotoRichi.MORI;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
